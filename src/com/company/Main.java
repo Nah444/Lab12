ba543e1f3671f9da7619804271c836922d9df65b
Nicholos Hatcher <nicholos.hatcher@gmail.com>
2017-07-27 09:15:50 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,52 @@
+package com.company;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class CarLot {
+    //set up an ArrayList, reference type is Car class, name of ArrayList is Lot
+    private ArrayList<Car> Lot = new ArrayList<Car>();
+
+    //this method creates a new instance of Car based on user input(from main)
+    public void addCar(String mk, String ml, int y, double pc) {
+        //instantiates a new Car object c based on the arguments provided
+        Car c = new Car(mk, ml, y, pc);
+        //adds Car object c to Lot ArrayList
+        Lot.add(c);
+    }
+
+    //this method creates a new instance of UsedCar based on user input (from main)
+    public void addCar(String mk, String ml, int y, double pc, String u, double m) {
+        //instantiates a new UsedCar object c based on the arguments provided
+        UsedCar uc = new UsedCar(mk, ml, y, pc, u, m);
+        //adds UsedCar object uc to Lot ArrayList
+        Lot.add(uc);
+    }
+    public void getCar(int id) {
+        Lot.get(id);
+    }
+
+    public void getCars () {
+        int i;
+        for (i = 0; i <= (Lot.size() - 1); i++) {
+            System.out.println((i + 1) + ": " + Lot.get(i));
+        }
+    }
+
+    public ArrayList <Car> returnList (){
+        return Lot;
+        //keep in mind cloning objects
+    }
+
+    public Car getCars (int userInput) {
+        System.out.println(Lot.get(userInput-1));
+        return Lot.get(userInput-1);
+    }
+
+    public void removeCars (int userInput){
+        Lot.remove(userInput - 1);
+        getCars();
+    }
+
+
+}
